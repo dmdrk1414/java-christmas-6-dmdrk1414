f6429c1c11693bfa5cb0f1ef9245e090bc345821
박승찬 <seungchan141414@gmail.com>
2023-11-11 13:52:22 +0900

    [Test(lotto)] 주문 날짜에 따른 주말 할인 해택 가능 여부 기능 테스트

--- src/test/java/christmas/domain/WeekendDayDiscountTest.java
+++ src/test/java/christmas/domain/WeekendDayDiscountTest.java
@@ -36,4 +36,61 @@ class WeekendDayDiscountTest {
         // then
         assertThat(result).isEqualTo(amount);
     }
+
+    @DisplayName("해당 주문 날짜가 주말 할인 행사인지 확인하는 기능 ")
+    @ParameterizedTest
+    @CsvSource({
+            // 주말
+            "1, true",
+            "2, true",
+
+            "8, true",
+            "9, true",
+
+            "15, true",
+            "16, true",
+
+            "22, true",
+            "23, true",
+
+            "29, true",
+            "30, true",
+            
+            // 평일
+            "3, false",
+            "4, false",
+            "5, false",
+            "6, false",
+            "7, false",
+
+            "10, false",
+            "11, false",
+            "12, false",
+            "13, false",
+            "14, false",
+
+            "17, false",
+            "18, false",
+            "19, false",
+            "20, false",
+            "21, false",
+
+            "24, false",
+            "25, false",
+            "26, false",
+            "27, false",
+            "28, false",
+
+            "31, false",
+    })
+    void checkPeriod(Integer orderDay, Boolean confirm) {
+        // given
+        WeekendDayDiscount weekendDayDiscount = new WeekendDayDiscount(orderDay);
+
+        // when
+        Boolean result = weekendDayDiscount.checkPeriod();
+
+        // than
+        assertThat(result).isEqualTo(confirm);
+    }
 }
