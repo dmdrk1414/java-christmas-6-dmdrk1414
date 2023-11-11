bebfec5cbacf36f0e6970836e04f022ecf9f82f9
박승찬 <seungchan141414@gmail.com>
2023-11-11 16:28:24 +0900

    [Feat(lotto)] 객체 생성 Config 클래스 추가.

---
+++ src/main/java/christmas/config/Config.java
@@ -0,0 +1,24 @@
+package christmas.config;
+
+import christmas.domain.discount.DDayDiscount;
+import christmas.domain.discount.SpecialDayDiscount;
+import christmas.domain.discount.WeekDayDiscount;
+import christmas.domain.discount.WeekendDayDiscount;
+
+public class Config {
+    public static DDayDiscount dDayDiscount(Integer orderDay) {
+        return new DDayDiscount(orderDay);
+    }
+
+    public static WeekDayDiscount weekDayDiscount(Integer orderDay, Integer dessertCount) {
+        return new WeekDayDiscount(orderDay, dessertCount);
+    }
+
+    public static WeekendDayDiscount weekendDayDiscount(Integer orderDay) {
+        return new WeekendDayDiscount(orderDay);
+    }
+
+    public static SpecialDayDiscount specialDayDiscount(Integer orderDay) {
+        return new SpecialDayDiscount(orderDay);
+    }
+}
