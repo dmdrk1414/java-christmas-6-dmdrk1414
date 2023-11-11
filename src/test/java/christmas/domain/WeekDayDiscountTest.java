2eb2204f57457360afea116848ddad3080d94bfe
박승찬 <seungchan141414@gmail.com>
2023-11-11 13:21:24 +0900

    [Test(lotto)] 주문 날짜에 따른 평일 할인 해택 금액을 알려주는 기능 테스트

---
+++ src/test/java/christmas/domain/WeekDayDiscountTest.java
@@ -0,0 +1,56 @@
+package christmas.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class WeekDayDiscountTest {
+
+    @DisplayName("주문 날짜에 따른 평일 할인 해택 금액을 알려주는 기능")
+    @ParameterizedTest
+    @CsvSource({
+            "3, 2023",
+            "4, 2023",
+            "5, 2023",
+            "6, 2023",
+            "7, 2023",
+
+            "10, 2023",
+            "11, 2023",
+            "12, 2023",
+            "13, 2023",
+            "14, 2023",
+
+            "17, 2023",
+            "18, 2023",
+            "19, 2023",
+            "20, 2023",
+            "21, 2023",
+
+            "24, 2023",
+            "25, 2023",
+            "26, 2023",
+            "27, 2023",
+            "28, 2023",
+
+            "31, 2023"
+    })
+    void giveAmount(Integer orderDay, Integer amount) {
+        System.out.println("orderDay = " + orderDay);
+        System.out.println("amount = " + amount);
+        WeekDayDiscount weekDayDiscount = new WeekDayDiscount(orderDay);
+        // when
+        Integer result = weekDayDiscount.giveAmount();
+
+        // then
+        assertThat(result).isEqualTo(amount);
+    }
+
+    @Test
+    void checkPeriod() {
+    }
+}
