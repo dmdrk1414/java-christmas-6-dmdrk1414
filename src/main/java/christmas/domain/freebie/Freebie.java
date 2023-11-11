f9627e3e826e905a501a90573f349e33238a0ca7
박승찬 <seungchan141414@gmail.com>
2023-11-12 08:48:39 +0900

    [Feat(christmas)]  증정품의  정보를 얻는 기능 추가

--- src/main/java/christmas/domain/freebie/Freebie.java
+++ src/main/java/christmas/domain/freebie/Freebie.java
@@ -1,10 +1,14 @@
 package christmas.domain.freebie;
 
 import christmas.constant.menu.Menu;
+import christmas.constant.menu.MenuGroup;
 import christmas.domain.Orders;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Freebie {
-    private static final Integer CONDITION_SHAMPAGNE_ORDER_MONEY = 12_000;
+    private static final Integer CONDITION_SHAMPAGNE_ORDER_MONEY = 12_0000;
     private String champagne;
 
     public Freebie() {
@@ -16,6 +20,8 @@ public class Freebie {
     }
 
     public Boolean isEligible(Orders orders) {
+        System.out.println("orders.getOrderMoney() = " + orders.getOrderMoney());
         return orders.getOrderMoney() >= CONDITION_SHAMPAGNE_ORDER_MONEY;
     }
+
 }
