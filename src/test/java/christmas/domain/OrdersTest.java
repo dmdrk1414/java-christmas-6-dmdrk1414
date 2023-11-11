b22363b787d361590435a44199dcab4da9f6d4bc
박승찬 <seungchan141414@gmail.com>
2023-11-11 20:24:28 +0900

    [Test(lotto)] 해당 메뉴가 있는지 확인하는 enum 메서드 테스트 추가

---
+++ src/test/java/christmas/domain/OrdersTest.java
@@ -0,0 +1,36 @@
+package christmas.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrdersTest {
+    @BeforeEach
+    void setUp() {
+    }
+
+    @DisplayName("주문의 에피타이저 갯수를 반환하는 기능 수정")
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "티본스테이크-1,바비큐립-1,양송이수프-2,초코케이크-2,제로콜라-1",
+            "티본스테이크-1,바비큐립-1,양송이수프-1,타파스-1,초코케이크-2,제로콜라-1",
+            "티본스테이크-1,바비큐립-1,시저샐러드-1,양송이수프-1,초코케이크-2,제로콜라-1",
+            "티본스테이크-1,바비큐립-1,타파스-1,시저샐러드-1,초코케이크-2,제로콜라-1",
+    })
+    void getAppetizerCount(String ordersString) {
+        // given
+        Orders orders = new Orders(ordersString);
+
+        // when
+        Integer result = orders.getAppetizerCount();
+
+        // then
+        assertThat(result).isEqualTo(2);
+    }
+}
