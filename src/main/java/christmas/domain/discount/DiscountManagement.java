1a68e3f057b4db4ff421fe006ecf67ba20c518af
박승찬 <seungchan141414@gmail.com>
2023-11-11 17:19:23 +0900

    [Feat(lotto)] 주문날짜의 총 할인 금액을 얻는 기능 구현

--- src/main/java/christmas/domain/discount/DiscountManagement.java
+++ src/main/java/christmas/domain/discount/DiscountManagement.java
@@ -1,4 +1,37 @@
 package christmas.domain.discount;
 
+import christmas.config.Config;
+import christmas.domain.Order;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class DiscountManagement {
+    private List<Discount> discountManagement;
+
+    // TODO: 11/11/23 매개변수 생각 dessert와 메인 메뉴관리
+    public DiscountManagement(Integer orderDay, Order order) {
+        discountManagement = List.of(
+                Config.dDayDiscount(orderDay),
+                Config.weekDayDiscount(orderDay, order.getDessertCount()),
+                Config.weekendDayDiscount(orderDay, order.getMainCount()),
+                Config.specialDayDiscount(orderDay)
+        );
+    }
+
+    public Integer getTotalDiscount() {
+        Integer totalDiscount = discountManagement.stream()
+                .map(Discount::giveAmount)
+                .reduce(0, Integer::sum);
+
+        // TODO: 11/11/23 삭제
+        discountManagement.stream()
+                .forEach(discount -> System.out.println(discount.giveAmount()));
+
+        discountManagement.stream()
+                .peek(discount -> System.out.println(discount.giveAmount()));
+
+        return totalDiscount;
+    }
 }
