5af6242ba72a614d5d13729a56c16088e2f430b8
박승찬 <seungchan141414@gmail.com>
2023-11-11 20:38:58 +0900

    [Test(lotto)] 주문의 메인 갯수를 반환하는 기능 테스트

--- src/test/java/christmas/domain/OrdersTest.java
+++ src/test/java/christmas/domain/OrdersTest.java
@@ -33,4 +33,25 @@ class OrdersTest {
         // then
         assertThat(result).isEqualTo(2);
     }
+
+    @DisplayName("주문의 메인 갯수를 반환하는 기능 수정")
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "티본스테이크-1,바비큐립-1,양송이수프-2,초코케이크-2,제로콜라-1",
+            "바비큐립-1,해산물파스타-1,양송이수프-2,초코케이크-2,제로콜라-1",
+            "해산물파스타-1,크리스마스파스타-1,양송이수프-2,초코케이크-2,제로콜라-1",
+            "바비큐립-1,해산물파스타-1,양송이수프-2,초코케이크-2,제로콜라-1",
+            "해산물파스타-1,크리스마스파스타-1,양송이수프-2,초코케이크-2,제로콜라-1",
+            "크리스마스파스타-1,티본스테이크-1,양송이수프-2,초코케이크-2,제로콜라-1",
+            "티본스테이크-1,해산물파스타-1,양송이수프-2,초코케이크-2,제로콜라-1",
+    })
+    void getMainCount(String ordersString) {
+        Orders orders = new Orders(ordersString);
+
+        // when
+        Integer result = orders.getMainCount();
+
+        // then
+        assertThat(result).isEqualTo(2);
+    }
 }
