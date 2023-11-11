9125e7dce3d314de1c4e31f1a7d5845dc4f0531c
박승찬 <seungchan141414@gmail.com>
2023-11-11 16:27:30 +0900

    [Fix(lotto)] 평일 할인 디저트 메뉴 갯수만큼 으로 수정

--- src/main/java/christmas/domain/discount/WeekDayDiscount.java
+++ src/main/java/christmas/domain/discount/WeekDayDiscount.java
@@ -3,13 +3,16 @@ package christmas.domain.discount;
 import christmas.constant.discount.DiscountGroup;
 
 public class WeekDayDiscount extends Discount {
-    public WeekDayDiscount(Integer orderDay) {
+    private Integer dessertOrderCount;
+
+    public WeekDayDiscount(Integer orderDay, Integer dessertCount) {
         super(orderDay);
+        this.dessertOrderCount = dessertCount;
     }
 
     @Override
     public Integer giveAmount() {
-        Integer amount = super.getAmount(DiscountGroup.WEEK_DAY_DISCOUNT);
+        Integer amount = super.getAmount(DiscountGroup.WEEK_DAY_DISCOUNT) * dessertOrderCount;
 
         return amount;
     }
