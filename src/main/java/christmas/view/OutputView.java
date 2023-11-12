cd8e9f7dd795c124293c4d99022ece0fb4d3b866
박승찬 <seungchan141414@gmail.com>
2023-11-12 22:08:56 +0900

    [Feat(christmas)] 선택한 날짜에 이벤트를 먼저 보는 기능 추가

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -4,4 +4,9 @@ public class OutputView {
     public void printIntroduce() {
         System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
     }
+
+    public void printEventPreview(Integer orderDate) {
+        System.out.println("12월 " + orderDate + "일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!");
+        System.out.println();
+    }
 }
