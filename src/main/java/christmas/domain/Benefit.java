2773b4238774ac9a8acdfcf3dbf4ec639fc9d7f7
박승찬 <seungchan141414@gmail.com>
2023-11-12 10:13:06 +0900

    [Feat(christmas)] 해택의 증정 상품을 받을수 있는가 확인하는 기능 추가.

--- src/main/java/christmas/domain/Benefit.java
+++ src/main/java/christmas/domain/Benefit.java
@@ -24,6 +24,7 @@ public class Benefit {
     // 평일 할인: -4,046원
     // 특별 할인: -1,000원
     // 증정 이벤트: -25,000원
+    // TODO: 11/12/23 order객체의 종속 관계 확인
     public Map<String, Integer> generateInformations() {
         Map<String, Integer> discountInformations = discountManagement.getInformations();
         Map<String, Integer> freebieInformations = freebie.getInformations(orders);
@@ -36,6 +37,10 @@ public class Benefit {
     }
 
 
-    // TODO: 11/12/23 샴페인 1개 증정
+    public Boolean isEligibleFreebie() {
+        return freebie.isEligible(orders);
+    }
+
     // TODO: 11/12/23 총해택 금액 (샴페인 추가, 할인)
+
 }
