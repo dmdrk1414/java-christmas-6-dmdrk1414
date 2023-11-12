7cb1f4dc534612a6bc1b3fcba36fc9042ba182bf
박승찬 <seungchan141414@gmail.com>
2023-11-12 22:12:22 +0900

    [Refactor(christmas)] 메서드 분리

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -2,11 +2,19 @@ package christmas.view;
 
 public class OutputView {
     public void printIntroduce() {
-        System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
+        print("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
     }
 
     public void printEventPreview(Integer orderDate) {
-        System.out.println("12월 " + orderDate + "일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!");
+        print("12월 " + orderDate + "일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!");
+        printNewLine();
+    }
+
+    private void print(String string) {
+        System.out.println(string);
+    }
+
+    private void printNewLine() {
         System.out.println();
     }
 }
