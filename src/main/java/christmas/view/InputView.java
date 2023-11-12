87c6d338a295493f8af591031139d4ae0def1cd6
박승찬 <seungchan141414@gmail.com>
2023-11-13 00:15:18 +0900

    [Style(christmas)] 주석 추가

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -4,8 +4,9 @@ import camp.nextstep.edu.missionutils.Console;
 import christmas.utill.CommonUtils;
 
 public class InputView {
+    
 
-    // TODO: 11/12/23 검증하기 
+    // TODO: 11/12/23 검증하기
     public int readDate() {
         System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
         Integer orderDate = CommonUtils.parsInt(Console.readLine());
