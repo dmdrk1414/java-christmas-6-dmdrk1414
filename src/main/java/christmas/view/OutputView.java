aa63bd45059e27212ee18b4a8baaa38a3a3d3631
박승찬 <seungchan141414@gmail.com>
2023-11-12 22:18:11 +0900

    [Refactor(christmas)] 메서드 이름 변경

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -4,11 +4,11 @@ import java.util.Map;
 
 public class OutputView {
     public void printIntroduce() {
-        print("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
+        println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
     }
 
     public void printEventPreview(Integer orderDate) {
-        print("12월 " + orderDate + "일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!");
+        println("12월 " + orderDate + "일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!");
         printNewLine();
     }
 
@@ -17,11 +17,11 @@ public class OutputView {
             String menu = entry.getKey();
             Integer quantity = entry.getValue();
 
-            print(menu + " " + quantity + "개");
+            println(menu + " " + quantity + "개");
         }
     }
 
-    private void print(String string) {
+    private void println(String string) {
         System.out.println(string);
     }
 
