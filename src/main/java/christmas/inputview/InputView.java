b3e7b80d287ac7fbd26b5c26b39abd615a6f38c0
박승찬 <seungchan141414@gmail.com>
2023-11-12 21:50:50 +0900

    [Feat(christmas)] 주문 메뉴을 입력하는 기능 추가

--- src/main/java/christmas/inputview/InputView.java
+++ src/main/java/christmas/inputview/InputView.java
@@ -12,4 +12,12 @@ public class InputView {
 
         return orderDate;
     }
+
+    // TODO: 11/12/23 검증하기
+    public String readOrder() {
+        System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+        String orderString = Console.readLine();
+
+        return orderString;
+    }
 }
