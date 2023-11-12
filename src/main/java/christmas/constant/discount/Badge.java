d06b64212e9a368dc33549f8c93e742173c91d9f
박승찬 <seungchan141414@gmail.com>
2023-11-12 11:43:47 +0900

    [Feat(christmas)] 뱃지 천체 할인가격에 따른 뱃지 이름 반환

--- src/main/java/christmas/constant/discount/Badge.java
+++ src/main/java/christmas/constant/discount/Badge.java
@@ -5,11 +5,49 @@ public enum Badge {
     TREE("트리", 10_000),
     SANTA("산타", 20_000);
 
+    private static final String NOTTING = "없음";
     private String name;
     private Integer condition;
 
     Badge(String name, Integer condition) {
         this.name = name;
         this.condition = condition;
+    }
+
+    public static String getBatName(Integer totalDiscount) {
+        if (isSanta(totalDiscount)) {
+            return SANTA.getName();
+        }
+        if (isTree(totalDiscount)) {
+            return TREE.getName();
+        }
+        if (isStar(totalDiscount)) {
+            return STAR.getName();
+        }
+        return NOTTING;
+    }
+
+    private static Boolean isStar(Integer totalDiscount) {
+        return totalDiscount >= STAR.condition;
+    }
+
+    private static Boolean isTree(Integer totalDiscount) {
+        return totalDiscount >= TREE.condition;
+    }
+
+    private static Boolean isSanta(Integer totalDiscount) {
+        return totalDiscount >= SANTA.condition;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getCondition() {
+        return condition;
+    }
+
+    public Boolean isCondition(Integer totalDiscount) {
+        return totalDiscount >= getCondition();
     }
 }
