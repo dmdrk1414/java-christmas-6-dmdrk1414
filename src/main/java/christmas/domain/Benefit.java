e60fff2be170e02969fe4be5f247f0652ff47aaa
박승찬 <seungchan141414@gmail.com>
2023-11-12 10:53:31 +0900

    [Style(christmas)] 주석을 삭제

--- src/main/java/christmas/domain/Benefit.java
+++ src/main/java/christmas/domain/Benefit.java
@@ -18,12 +18,6 @@ public class Benefit {
         this.orders = orders;
     }
 
-    // TODO: 11/12/23 해택이름 (할인과 증정품 반환)
-    // <혜택 내역>
-    // 크리스마스 디데이 할인: -1,200원
-    // 평일 할인: -4,046원
-    // 특별 할인: -1,000원
-    // 증정 이벤트: -25,000원
     // TODO: 11/12/23 order객체의 종속 관계 확인
     public Map<String, Integer> generateInformations() {
         Map<String, Integer> discountInformations = discountManagement.getInformations();
@@ -41,7 +35,6 @@ public class Benefit {
         return freebie.isEligible(orders);
     }
 
-    // TODO: 11/12/23 총해택 금액 (샴페인 추가, 할인)
     public Integer getTotalAmount() {
         Integer totalAmount = 0;
 
