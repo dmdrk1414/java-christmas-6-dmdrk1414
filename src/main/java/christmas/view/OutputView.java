eb1948c7b1b065ea10b0e92e9c864955b550290a
박승찬 <seungchan141414@gmail.com>
2023-11-12 22:17:24 +0900

    [Feat(christmas)] 주문 메뉴을 출력하는 기능 추가

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,5 +1,7 @@
 package christmas.view;
 
+import java.util.Map;
+
 public class OutputView {
     public void printIntroduce() {
         print("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
@@ -8,6 +10,15 @@ public class OutputView {
     public void printEventPreview(Integer orderDate) {
         print("12월 " + orderDate + "일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!");
         printNewLine();
+    }
+
+    public void printOrderMenu(Map<String, Integer> orders) {
+        for (Map.Entry<String, Integer> entry : orders.entrySet()) {
+            String menu = entry.getKey();
+            Integer quantity = entry.getValue();
+
+            print(menu + " " + quantity + "개");
+        }
     }
 
     private void print(String string) {
