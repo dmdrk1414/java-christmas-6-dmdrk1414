51177fb8ec56a972a4ae899fac8c618b7e894bd8
박승찬 <seungchan141414@gmail.com>
2023-11-13 10:17:50 +0900

    [Feat(christmas)] 공백 입력 검사 검증 추가

--- src/main/java/christmas/validate/InputValidate.java
+++ src/main/java/christmas/validate/InputValidate.java
@@ -1,11 +1,22 @@
 package christmas.validate;
 
+import christmas.constant.view.InputConstant;
+
 public class InputValidate {
+    private static final String NOTING_STRING = "";
+
     public static void orderDateRange(String date) {
         Integer dateNum = parseInt(date);
         if (dateNum < 1 || dateNum > 31) {
             throw new NumberFormatException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
         }
+    }
+
+    public static Boolean blankCheck(String strLine) {
+        if (!strLine.equals(NOTING_STRING)) {
+            return true;
+        }
+        throw new NumberFormatException(InputConstant.ERROR_BLANK_CHECK.getMSG());
     }
 
     private static Integer parseInt(String date) {
