f0277a3681fd2ae507d0d7616c46aea8669e4408
박승찬 <seungchan141414@gmail.com>
2023-11-13 14:57:23 +0900

    [Test(christmas)] 문자열, 특수문자 포함 검사 테스트

--- src/test/java/christmas/ApplicationTest.java
+++ src/test/java/christmas/ApplicationTest.java
@@ -4,6 +4,7 @@ import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -28,6 +29,7 @@ class ApplicationTest extends NsTest {
         });
     }
 
+    @Disabled
     @Test
     void 혜택_내역_없음_출력() {
         assertSimpleTest(() -> {
@@ -44,6 +46,7 @@ class ApplicationTest extends NsTest {
         });
     }
 
+    @Disabled
     @Test
     void 주문_예외_테스트() {
         assertSimpleTest(() -> {
@@ -65,6 +68,15 @@ class ApplicationTest extends NsTest {
     void 날짜_예외_테스트_공백포함_테스트() {
         assertSimpleTest(() -> {
             runException("1 3");
+            assertThat(output()).contains("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        });
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"a", "b", "1a", "1박", "박승", "!2", "q!1", "@#$", "박&*("})
+    void 날짜_예외_테스트_문자열_테스트() {
+        assertSimpleTest(() -> {
+            runException("a");
             assertThat(output()).contains("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
         });
     }
