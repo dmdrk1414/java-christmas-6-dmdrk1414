0a00e7024ccfc4fd0b9d1f0cc86349693217310a
박승찬 <seungchan141414@gmail.com>
2023-11-13 13:55:59 +0900

    [Refactor(christmas)] 검증 관리 상수 이름 변경

---
+++ src/main/java/christmas/constant/validate/ValidateConstant.java
@@ -0,0 +1,15 @@
+package christmas.constant.validate;
+
+public enum ValidateConstant {
+    ERROR_ORDER_DATE("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+
+    private String constant;
+
+    ValidateConstant(String constant) {
+        this.constant = constant;
+    }
+
+    public String getMSG() {
+        return constant;
+    }
+}
