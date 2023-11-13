0900623eb923386e96cd8fd11d515479f9d5d6d2
박승찬 <seungchan141414@gmail.com>
2023-11-13 13:55:41 +0900

    [Refactor(christmas)] 메서드 분리

--- src/main/java/christmas/validate/InputValidate.java
+++ src/main/java/christmas/validate/InputValidate.java
@@ -1,23 +1,18 @@
 package christmas.validate;
 
-import christmas.constant.view.InputConstant;
+import christmas.constant.validate.ValidateConstant;
 
 public class InputValidate {
-    private static final String NOTING_STRING = "";
-
     public static void orderDateRange(String date) {
         Integer dateNum = parseInt(date);
+        
         if (dateNum < 1 || dateNum > 31) {
-            throw new NumberFormatException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+            thorwNumberFormatExceptionAboutOrderDate();
         }
     }
 
-    // TODO: 11/13/23 검증 적용 
-    public static Boolean blankCheck(String strLine) {
-        if (!strLine.equals(NOTING_STRING)) {
-            return true;
-        }
-        throw new NumberFormatException(InputConstant.ERROR_BLANK_CHECK.getMSG());
+    private static void thorwNumberFormatExceptionAboutOrderDate() {
+        throw new NumberFormatException(ValidateConstant.ERROR_ORDER_DATE.getMSG());
     }
 
     private static Integer parseInt(String date) {
