1f0b99efa3cbc3bab1f355d030c1090ddcc14082
박승찬 <seungchan141414@gmail.com>
2023-11-14 20:50:36 +0900

    [Test(christmas)] 주문_정보을_확인한다

--- src/test/java/christmas/domain/ManagementTest.java
+++ src/test/java/christmas/domain/ManagementTest.java
@@ -8,9 +8,14 @@ import christmas.domain.order.Orders;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ManagementTest {
+    private Management management;
     private BadgeManagement badgeManagement;
     private PaymentCalculator paymentCalculator;
     private Benefit benefit;
@@ -18,18 +23,27 @@ class ManagementTest {
 
     @BeforeEach
     void setUp() {
+        this.management = new Management();
         this.badgeManagement = Config.badgeManagement();
         this.paymentCalculator = Config.paymentCalculator();
     }
 
     @Test
-    void getOrderInformation() {
+    void 주문_정보을_확인한다() {
         // given
+        String orderString = "티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1";
+        Integer orderDay = 3;
+        Map<String, Integer> target = new HashMap<>();
+        target.put("티본스테이크", 1);
+        target.put("바비큐립", 1);
+        target.put("초코케이크", 2);
+        target.put("제로콜라", 1);
 
         // when
+        Map<String, Integer> result = management.getOrderInformation(orderDay, orderString);
 
         // than
-
+        assertThat(result).isEqualTo(target);
     }
 
     @Test
