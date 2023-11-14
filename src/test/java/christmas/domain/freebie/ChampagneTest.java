4ad57a7670e317fe49efb850e4cc29604c69d3b6
박승찬 <seungchan141414@gmail.com>
2023-11-14 19:47:57 +0900

    [Test(christmas)] 증정품 샴페인 테스트 코드 추가

---
+++ src/test/java/christmas/domain/freebie/ChampagneTest.java
@@ -0,0 +1,49 @@
+package christmas.domain.freebie;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ChampagneTest {
+    @BeforeEach
+    void setUp() {
+    }
+
+
+    @Test
+    void makeChampagneCountInformation() {
+        // given
+
+
+        // when
+
+
+        // then
+
+    }
+
+    @Test
+    void isEligible() {
+        // given
+
+
+        // when
+
+
+        // then
+
+    }
+
+    @Test
+    void getPrice() {
+        // given
+
+
+        // when
+
+
+        // then
+
+    }
+}
