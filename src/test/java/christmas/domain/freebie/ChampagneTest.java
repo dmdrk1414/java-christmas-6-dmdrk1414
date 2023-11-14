50f6ef5d8733c8a6ae775f548bbedd294699ffb9
박승찬 <seungchan141414@gmail.com>
2023-11-14 19:53:01 +0900

    [Test(christmas)] 샴페인의 정보를 Map형태로 확인한다

--- src/test/java/christmas/domain/freebie/ChampagneTest.java
+++ src/test/java/christmas/domain/freebie/ChampagneTest.java
@@ -1,26 +1,36 @@
 package christmas.domain.freebie;
 
+import christmas.constant.freebie.Freebies;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ChampagneTest {
-    
+    private Champagne champagne;
+
     @BeforeEach
     void setUp() {
+        champagne = new Champagne();
     }
 
+    @DisplayName("샴페인의 정보를 Map형태로 확인한다")
     @Test
     void makeChampagneCountInformation() {
         // given
-
+        Map<String, Integer> target = new HashMap<>();
+        target.put(Freebies.CHAMPAGNE.getName(), Freebies.CHAMPAGNE.getCount());
 
         // when
-
+        Map<String, Integer> result = champagne.makeChampagneCountInformation();
 
         // then
-
+        assertThat(result).isEqualTo(target);
     }
 
     @Test
