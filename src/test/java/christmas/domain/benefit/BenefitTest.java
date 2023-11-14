26c2df009e1158205da9f19077a1b8ee939be86d
박승찬 <seungchan141414@gmail.com>
2023-11-14 18:06:19 +0900

    [Test(christmas)] 날짜 기반 할인 총 액수를 확인한다

--- src/test/java/christmas/domain/benefit/BenefitTest.java
+++ src/test/java/christmas/domain/benefit/BenefitTest.java
@@ -1,9 +1,12 @@
 package christmas.domain.benefit;
 
 import christmas.domain.order.Orders;
+import christmas.domain.testutill.TestUtill;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.Map;
 
@@ -50,11 +53,23 @@ class BenefitTest {
         assertThat(result.toString()).contains("{}");
     }
 
-    @Test
-    void getTotalDiscount() {
+    @DisplayName("날짜 기반 할인 총 액수를 확인한다.")
+    @ParameterizedTest
+    @CsvSource({
+            "티본스테이크-1바비큐립-1초코케이크-2제로콜라-1, 6246",
+            "타파스-1제로콜라-1, 0"
+    })
+    void getTotalDiscount(String orderString, Integer totalDiscount) {
         // given
+        orderString = TestUtill.insertComma(orderString);
+        Orders orders = new Orders(orderString);
+        benefit = new Benefit(3, orders);
+
         // when
+        Integer result = benefit.getTotalDiscount();
+
         // than
+        assertThat(result).isEqualTo(totalDiscount);
     }
 
     @Test
